da0dcc9bdca524992fcb363e28d6f2a13e5dc1ea
PaulKlein22 <paulklein22@gmail.com>
2018-11-12 01:43:00 -0600

    final update

--- liri.js
+++ liri.js
@@ -1,90 +1,79 @@
-////////////////////////////////
-/////// ES6 THIS SHIT
-///////////////////////////////
-
-
-
-// Make it so liri.js can take in one of the following commands:
-// concert-this
-// spotify-this-song
-// movie-this
-// do-what-it-says
-
 // Add code to read and set any environment variables with the dotenv package:
 require("dotenv").config();
 
 // Add the code required to import the `keys.js` file and store it in a variable.
 var keys = require("./keys");
-var spotify = require("node-spotify-api");
+var Spotify = require("node-spotify-api");
 var request = require("request");
-var fs = require("fs");
-
-var spotify = new Spotify(keys.spotify);
-
-//grabbing user input variables
-var input = process.argv;
-
-//grabs user command for movies, spotify or random
-var command = input[2];
-
-/////////// Bands In Town ///////////////////////////////////////////////////////////////////////////////////
-
-// NODE REQUEST: node liri.js concert-this
-
-// This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
-// * Name of the venue
-// * Venue location
-// * Date of the Event (use moment to format this as "MM/DD/YYYY")
-
-/////////// Spotify /////////////////////////////////////////////////////////////////////////////////////////
-
-// NODE REQUEST: node liri.js spotify-this-song
-// If the user doesn't type in a song, the program will default to "The Sign" by Ace of Base.
-
-/////////// OMDB_Request ////////////////////////////////////////////////////////////////////////////////////
-
-// NODE REQUEST: node liri.js movie-this '<movie name here>'
-// If the user doesn't type in a movie, the program will default to the movie 'Mr. Nobody.'
-
-var queryURL =
-  "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
-request(queryURL, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    // Parse the body of the site and recover the following:
-    console.log("Title: " + JSON.parse(body).imdbRating);
-    console.log("Release: " + JSON.parse(body).imdbRating);
-    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).imdbRating);
-    console.log("Country: " + JSON.parse(body).imdbRating);
-    console.log("Language: " + JSON.parse(body).imdbRating);
-    console.log("Plot: " + JSON.parse(body).imdbRating);
-    console.log("Actors: " + JSON.parse(body).imdbRating);
-  }
+var moment = require("moment");
+var spotify = new Spotify({
+  id: keys.spotify.id,
+  secret: keys.spotify.secret
 });
 
-/////////// Do What it Says /////////////////////////////////////////////////////////////////////////////////
+// Bands In Town
+if (process.argv[2] == "concert-this") {
+  var artist = process.argv.slice(3).join(" ");
+  console.log(artist);
 
-// NODE REQUEST: node liri.js do-what-it-says
+  var queryURL =
+    "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-/////////// WeatherNPM //////////////////////////////////////////////////////////////////////////////////////
+  request(queryURL, function(error, response, body) {
+    if (error) console.log(error);
+    var result = JSON.parse(body)[0];
+    console.log("Venue: " + result.venue.name);
+    console.log("Location: " + result.venue.city);
+    console.log("Date: " + moment(result.datetime).format("MM/DD/YYYY"));
+  });
 
-// Here we include the weather-js so we can use it in our Node application.
-var weather = require("weather-js");
+  //Spotify
+} else if (process.argv[2] == "spotify-this-song") {
+  var songName = process.argv.slice(3).join(" ");
 
-// Then we use the package to search for the weather at a location
-weather.find({ search: "Anchorage, AK", degreeType: "F" }, function(
-  err,
-  result
-) {
-  // If there is an error log it.
-  if (err) {
-    console.log(err);
+  if (songName == undefined) {
+    songName = "The Sign by Ace of Base";
   }
 
-  // If there is no error... then print out the weather data.
-  // We use JSON.stringify to print the data in string format.
-  // We use the JSON.stringify argument of "2" to make the format pretty.
-  // See link here: http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
-  console.log(JSON.stringify(result, null, 2));
-});
+  spotify.search({type: "track", query: songName, limit: 10}, function(err, data) {
+    if (err) {
+      return console.log("Error Message: " + err);
+    }
+
+    for (var i = 0; i < data.tracks.items.length; i++) {
+      var result = {
+        artist: data.tracks.items[i].album.artists[0].name,
+        album_name: data.tracks.items[i].album.name,
+        song_name: data.tracks.items[i].name,
+        preview_url: data.tracks.items[i].preview_url
+      };
+    }
+  });
+
+  // OMDB
+} else if (process.argv[2] == "movie-this") {
+  var movieName = process.argv.slice(3).join(" ");
+
+  if (movieName == undefined) {
+    movieName = "Mr. Nobody";
+  }
+
+  request(
+    "http://www.omdbapi.com/?i=tt3896198&apikey=55e8eecb&t=" + process.argv[3],
+    function(error, response, body) {
+      var result = JSON.parse(body);
+      console.log("Title: " + result.Title);
+      console.log("Year: " + result.Released);
+      console.log("IMDB Rating: " + result.imdbRating);
+      console.log("Rotten Tomatoes: " + result.Ratings[1].Value);
+      console.log("Country: " + result.Country);
+      console.log("Language: " + result.Language);
+      console.log("Movie Plot: " + result.Plot);
+      console.log("Actors: " + result.Actors);
+    }
+  );
+
+  //Do What It Says
+} else if (process.argv[2] == "do-what-it-says") {
+  console.log("do what it says");
+};
